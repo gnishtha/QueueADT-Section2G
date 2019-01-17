0ee705693a222b92e1fecbba506accac0850b78b
gnishtha <nishtha.gupta_ccv17@gla.ac.in>
2019-01-17 09:47:30 +0530

    init commit

--- src/main/QueueMain.java
+++ src/main/QueueMain.java
@@ -5,7 +5,7 @@ public class QueueMain
 	public static void main(String[] args)
 	{
 		QueueADT queue1 = new QueueADT();
-
+			System.out.println(queue1.size());
 //		TODO add method call for enqueue
 
 //		TODO add method call for dequeue
